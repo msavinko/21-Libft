d2ffec49baabb41fe2f2934ad5318266bb8d4954
Meyers Arlean <marlean@at-i5.msk.21-school.ru>
2022-06-24 11:10:18 +0300

    new changes

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marlean <marlean@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rdanyell <rdanyell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:14:31 by marlean           #+#    #+#             */
-/*   Updated: 2022/02/14 16:39:06 by marlean          ###   ########.fr       */
+/*   Updated: 2022/06/16 15:56:50 by rdanyell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,14 @@ typedef struct s_list
 
 int		get_next_line(char **line);
 int		ft_atoi(const char *str);
+int		ft_isspace(char c);
 void	ft_bzero(void *str, size_t size);
 int		ft_isalnum(int ch);
 int		ft_isalpha(int ch);
 int		ft_isascii(int ch);
 int		ft_isdigit(int ch);
 int		ft_isprint(int ch);
-char	*ft_itoa(long int num);
+char	*ft_itoa(int ncd);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 void	*ft_memcpy(void *dest, const void *source, size_t n);
@@ -43,6 +44,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
